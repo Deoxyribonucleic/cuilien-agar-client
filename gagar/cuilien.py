d389d8070886034b81a6cc747706d6852d2288c5
Nils Ceberg <nils.ceberg@gmail.com>
2015-09-24 13:38:41 +0200

    Add cuilien infrastructure

--- gagar/cuilien.py
+++ gagar/cuilien.py
@@ -2,6 +2,7 @@ from .subscriber import MultiSubscriber, Subscriber
 from gi.repository import Gtk, GLib, Gdk
 import json
 import socket
+import struct
 
 class Bot(Subscriber):
     def __init__(self, client):
@@ -16,7 +17,19 @@ class Bot(Subscriber):
         print("sending target:", x, y)
         self.client.send_target(x, y)
 
+    def on_ingame(self):
+        self.conn.send_ready()
+
+    def on_respawn_command(self):
+        print("respawning!")
+        self.client.send_respawn()
+
+class MessageType:
+    ready = 1
+    respawn = 2
+
 class BotConnection():
+
     def __init__(self, address, subscriber):
         self.sub = subscriber
 
@@ -26,9 +39,22 @@ class BotConnection():
         self.watch()
 
     def on_message(self):
-        pos = json.loads(self.socket.recv(255).decode('utf8'))
-        print("now pos: ", str(pos))
-        self.sub.on_new_target(*pos)
+        length = struct.unpack("!H", self.socket.recv(2))[0]
+        raw_data = self.socket.recv(length).decode('utf8')
+        print(raw_data)
+
+        message = json.loads(raw_data)
+        message_type = message["type"]
+
+        if message_type == MessageType.respawn:
+            self.sub.on_respawn_command()
+
+    def send_ready(self):
+        self.send_message({ "type": MessageType.ready });
+
+    def send_message(self, msg):
+        data = json.dumps(msg).encode('utf8')
+        self.socket.send(struct.pack("!H", len(data)) + data)
 
     def watch(self):
         GLib.io_add_watch(self.socket, GLib.IO_IN, lambda ws, _: self.on_message() or True)
